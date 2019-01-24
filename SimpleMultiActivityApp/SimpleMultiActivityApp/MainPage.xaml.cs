207c9056776fdc6880e56e4f9a384ad85002b393
Piotr Kozak <piotr.kozak@pirios.com>
2019-01-24 16:58:23 +0100

    -działający kompas

--- SimpleMultiActivityApp/SimpleMultiActivityApp/MainPage.xaml.cs
+++ SimpleMultiActivityApp/SimpleMultiActivityApp/MainPage.xaml.cs
@@ -14,6 +14,26 @@ namespace SimpleMultiActivityApp
         public MainPage()
         {
             InitializeComponent();
+
+            Compass.ReadingChanged += Compass_ReadingChanged;
+        }
+
+        private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            LabelInfo.Text = $"Heading: {e.Reading.HeadingMagneticNorth}";
+            ImageArrow.Rotation = e.Reading.HeadingMagneticNorth;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Compass.Start(SensorSpeed.UI);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Compass.Stop();
         }
     }
 }
